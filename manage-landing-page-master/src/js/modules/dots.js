57a50fe22ef2c7afa7ca3411c50066c47346562f
jose33-afk <josedev337@gmail.com>
2025-09-26 21:52:53 -0300

    feat: bolinhas

---
+++ manage-landing-page-master/src/js/modules/dots.js
@@ -0,0 +1,18 @@
+const container = document.querySelector('.commts-contain > div');
+const dots = document.querySelectorAll('.dot');
+
+container.addEventListener('scroll', () => {
+  const scrollLeft = container.scrollLeft;
+  const itemWidth = 350;
+  const activeIndex = Math.round(scrollLeft / itemWidth); //1
+  
+  dots.forEach(dot => dot.classList.remove('active'));
+  
+  // Adiciona active na atual
+  if (dots[activeIndex]) {
+    dots[activeIndex].classList.add('active');
+  }
+});
+
+//Basicamnete ele pega a posicao do scroll e divide 
+//pela largura e depois aredonda pra o inteiro mais porximo.
