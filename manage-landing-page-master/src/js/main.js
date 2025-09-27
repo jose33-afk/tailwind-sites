a29b53185e0ed6f859d5220132f74ca3cdff4cf3
jose33-afk <josedev337@gmail.com>
2025-09-27 15:00:04 -0300

    feat: Versao mobile completa

--- manage-landing-page-master/src/js/main.js
+++ manage-landing-page-master/src/js/main.js
@@ -23,6 +23,13 @@ const menu = document.querySelector('.menu');
 const bntMenu = document.querySelector('#bnt-menu');
 
 bntMenu.addEventListener('click', () => {
+  const bntImg = bntMenu.querySelector('img');
+  const caminho = bntImg.getAttribute('src');
+
+  if (caminho === './src/img/icon-hamburger.svg') {
+    bntImg.src = './src/img/icon-close.svg';
+  } else bntImg.src = './src/img/icon-hamburger.svg';
+
   menu.classList.toggle('opacity-0');
   menu.classList.toggle('invisible');
   menu.classList.toggle('opacity-100');
